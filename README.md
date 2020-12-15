af5b18a9d5509a6e9934c2304cb2bd2069355a4b
mcberry23 <mattcastle38@gmail.com>
2020-12-14 21:25:38 -0600

    Initial commit.  Potential clock stretching issue.

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # ros_bno08x
+
+A ROS node for communicating with the [BNO080](https://www.sparkfun.com/products/14686) or [BNO085](https://www.adafruit.com/product/4754) 9DOF IMU.
+
+## Description
+
+The node communicates with the BNO08x via i2c on the Raspberry Pi using Adafruit's Python library: https://github.com/adafruit/Adafruit_CircuitPython_BNO08x.  The i2c address is preset to `0x4b` for the BNO080.  This is configurable for the BNO085 on `0x4a`.  The data is stored in the following:
+
+* Accelerometer and Gyroscope (sensor_msgs/Imu): `bno08x/raw`
+* Magnometer (sensor_msgs/MagneticField): `bno08x/mag`
+* Temperature (sensor_msgs/Temperature): `bno08x/temp`
+* Diagnostics (diagnostic_msgs/DiagnosticStatus): `bno08x/status`
+
+## Installation Instructions
+
+* Enable i2c
+  ```
+  sudo apt-get install i2c-tools
+  i2cdetect -l
+  ```
+* Add `i2c-devl` to boot with `sudo nano /etc/modules-load.d/modules.conf`
+* Install wiringpi `sudo apt install wiringpi`
+* Connect i2c devices to Sparkfun Qwiic hat and run `i2cdetect -y 1` to identify channels
+* Install Circuit Python: https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi
+* Install driver for BNO08x IMU: `sudo pip3 install adafruit-circuitpython-bno08x`.
+
+## Running the Node
+
+* Option 1: `rosrun ros_bno08x talker.py` 
+* Option 2: `roslaunch launches/bno08x.launch`
+  
+## Tested Setup
+
+It should work on other versions but Python 3 is a requirement.
+
+* Platform: Raspberry Pi 4
+* OS: Ubuntu MATE 20.04
+* ROS: Noetic
+* Python: 3.8.5
+
---
+++ src/talker.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# Driver: SPDX-FileCopyrightText: 2020 Bryan Siepert, written for Adafruit Industries
+
+import rospy
+from sensor_msgs.msg import MagneticField,Imu
+from std_msgs.msg import Float64
+from diagnostic_msgs.msg import DiagnosticStatus
+import time
+import sys
+import board
+import busio
+from adafruit_bno08x import (
+    BNO_REPORT_ACCELEROMETER,
+    BNO_REPORT_GYROSCOPE,
+    BNO_REPORT_MAGNETOMETER,
+    BNO_REPORT_ROTATION_VECTOR,
+)
+from adafruit_bno08x.i2c import BNO08X_I2C
+
+def bno08x_node():
+
+    # Initialize ROS node
+    raw_pub = rospy.Publisher('bno08x/raw', Imu, queue_size=10)
+    mag_pub = rospy.Publisher('bno08x/mag', MagneticField, queue_size=10)
+    status_pub = rospy.Publisher('bno08x/status', DiagnosticStatus, queue_size=10)
+    rospy.init_node('bno08x')
+    rate = rospy.Rate(100) # frequency in Hz
+    rospy.loginfo(rospy.get_caller_id() + "  bno08x node launched.")
+
+    i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
+    bno = BNO08X_I2C(i2c,address=0x4b) # BNO080 (0x4b) BNO085 (0x4a)
+
+    bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+    bno.enable_feature(BNO_REPORT_GYROSCOPE)
+    bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+    bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+    
+    time.sleep(0.5) # ensure IMU is initialized
+
+    while True:
+        raw_msg = Imu()
+        raw_msg.header.stamp = rospy.Time.now()
+
+        accel_x, accel_y, accel_z = bno.acceleration
+        raw_msg.linear_acceleration.x = accel_x
+        raw_msg.linear_acceleration.y = accel_y
+        raw_msg.linear_acceleration.z = accel_z
+
+        gyro_x, gyro_y, gyro_z = bno.gyro
+        raw_msg.angular_velocity.x = gyro_x
+        raw_msg.angular_velocity.y = gyro_y
+        raw_msg.angular_velocity.z = gyro_z
+        
+        quat_i, quat_j, quat_k, quat_real = bno.quaternion
+        raw_msg.orientation.w = quat_i
+        raw_msg.orientation.x = quat_j
+        raw_msg.orientation.y = quat_k
+        raw_msg.orientation.z = quat_real
+
+        
+        raw_msg.orientation_covariance[0] = -1
+        raw_msg.linear_acceleration_covariance[0] = -1
+        raw_msg.angular_velocity_covariance[0] = -1
+
+        raw_pub.publish(raw_msg)
+
+        mag_msg = MagneticField()
+        mag_x, mag_y, mag_z = bno.magnetic
+        mag_msg.header.stamp = rospy.Time.now()
+        mag_msg.magnetic_field.x = mag_x
+        mag_msg.magnetic_field.y = mag_y
+        mag_msg.magnetic_field.z = mag_z
+        mag_msg.magnetic_field_covariance[0] = -1
+        mag_pub.publish(mag_msg)
+        
+        status_msg = DiagnosticStatus()
+        status_msg.level = 0
+        status_msg.name = "bno08x IMU"
+        status_msg.message = ""
+        status_pub.publish(status_msg)
+
+        rate.sleep()   
+    
+    rospy.loginfo(rospy.get_caller_id() + "  bno08x node finished")
+
+if __name__ == '__main__':
+    try:
+        bno08x_node()
+    except rospy.ROSInterruptException:
+        rospy.loginfo(rospy.get_caller_id() + "  bno08x node exited with exception.")
